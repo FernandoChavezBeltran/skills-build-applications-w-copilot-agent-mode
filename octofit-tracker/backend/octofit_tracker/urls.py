1ab8b55f9fe3a50bc83cda23ee49baeb0a86c2b1
FernandoChavezBeltran <fernando.chavez@accenture.com>
2025-11-20 15:30:14 +0000

    octofit-tracker/backend/octofit_tracker/views.py octofit-tracker/backend/octofit_tracker/views.py

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -17,7 +17,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet, api_root
+
+from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+import os
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    base_url = request.build_absolute_uri('/')
+    if codespace_name:
+        base_url = f"https://{codespace_name}-8000.app.github.dev/"
+    return Response({
+        'users': f"{base_url}api/users/",
+        'teams': f"{base_url}api/teams/",
+        'activities': f"{base_url}api/activities/",
+        'leaderboard': f"{base_url}api/leaderboard/",
+        'workouts': f"{base_url}api/workouts/",
+    })
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
